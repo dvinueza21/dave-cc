0fd043b330c85fda23debb1859c387a50324ed7b
Dave Vinueza <dvinueza21@gmail.com>
2026-05-11 13:35:56 +0200

    login page actualizado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,124 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Page</title>
+
+
+    <style>
+        body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background: linear-gradient(135deg, #111, #2b2b2b);
+}
+
+.login-page {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-card {
+  width: 100%;
+  max-width: 600px;
+  background: #676262;
+  padding: 90px;
+ border-radius: 10px;
+  box-shadow: 0 20px 50px rgba(0,0,0,.25);
+}
+
+.login-card h1 {
+  font-size: 26px;
+  text-align: center;
+  margin-bottom: 35px;
+   color: #c8c6c6
+ 
+}
+
+.login-card h2 {
+  text-align: center;
+  font-size: 32px;
+  margin-bottom: 10px;
+   color: #c8c6c6
+   
+
+
+}
+
+.login-card p {
+  color: #f4f4f4;
+  margin-bottom: 30px;
+}
+
+.login-card input {
+  width: 50%;
+  height: 52px;
+  margin-bottom: 16px;
+  padding: 0 16px;
+  border: 1px solid #ddd;
+  border-radius: 12px;
+}
+
+.login-card input:focus {
+  outline: none;
+  border-color: #111;
+}
+
+.login-card button {
+  width: 80%;
+  height: 52px;
+  border: none;
+  border-radius: 12px;
+  background: #50ae7f;
+  color: white;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.login-card button:hover {
+  background: #333;
+}
+
+
+
+</style>
+
+
+</head>
+<body>
+    <section class="login-page">
+  <div class="login-card">
+
+    <h1>Hola Dave! </h1>
+    <h2></h2>
+    <p>Accede al panel de administración</p>
 
     <form method="POST" action="{{ route('login') }}">
-        @csrf
+      @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+      <input 
+        type="email" 
+        name="email" 
+        placeholder="Email" 
+        value="{{ old('email') }}"
+        required 
+        autofocus
+      >
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+      <input 
+        type="password" 
+        name="password" 
+        placeholder="Contraseña" 
+        required
+      >
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
+      <button type="submit">Iniciar sesión</button>
     </form>
-</x-guest-layout>
+
+  </div>
+</section>
+</body>
+</html>
